3743d43e4dd7a669324fa3b932c1857c53ce02cc
dimasadityamukhsinin <dimasadityamukhsinin@gmail.com>
2021-04-08 23:06:36 +0700

    update

--- functions/shopify.js
+++ functions/shopify.js
@@ -116,6 +116,7 @@ exports.handler = async (event, context) => {
             }
           })
         })
+        console.log(data.variants)
 
         if (data.variants.length >= 1) {
           hasVariantsToSync = true;

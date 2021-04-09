f18e185b82f679748ab7448e62a0b91ad22b418b
dimasadityamukhsinin <dimasadityamukhsinin@gmail.com>
2021-04-09 08:59:13 +0700

    update

--- functions/shopify.js
+++ functions/shopify.js
@@ -109,7 +109,6 @@ exports.handler = async (event, context) => {
               .patch(cv._id, (patch) => patch.set({ deleted: true }))
               .commit()
               .then((deletedObject) => {
-                console.log(deletedObject)
                 console.log(`successfully marked variant ${data.id} as 'deleted'`);
                 return deletedObject;
               })
